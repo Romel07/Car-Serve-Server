5f3ff03ebaeca66675c67a5bfb046629bea55a48
unknown <romel.barua@youngonectg.com>
2021-06-14 00:02:55 +0600

    addedApi

--- index.js
+++ index.js
@@ -52,7 +52,6 @@ app.post('/addReview', (req, res)=>{
     })
 })
 
-
 app.delete('/delete/:id', (req, res)=>{
   serviceCollection.deleteOne({_id: ObjectId(req.params.id)})
   .then(result => {
@@ -64,8 +63,6 @@ app.delete('/delete/:id', (req, res)=>{
 });
 
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+
 
 app.listen(process.env.PORT || port)
